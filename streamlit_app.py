6cad6d6cec921f595478b019a068c27c61b107df
Enock Wafula Bereka <enochosenwafulah@gmail.com>
2025-05-30 21:20:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,26 @@ st.write('This tool is designed to make real world prediction of Diabetes Outcom
 
 # Load saved models
 diabetes_model = pickle.load(open(diabetes_model.sav"), 'rb')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    pregnancies = st.number_input("Number of Pregnancies", min_value=0)
+with col2:
+    glucose = st.number_input("Glucose Level")
+with col3:
+    blood_pressure = st.number_input("Blood Pressure value")
+
+with col1:
+    skin_thickness = st.number_input("Skin Thickness value")
+with col2:
+    insulin = st.number_input("Insulin Level")
+with col3:
+    bmi = st.number_input("BMI value")
+
+with col1:
+    diabetes_pedigree = st.number_input("Diabetes Pedigree Function value")
+with col2:
+    age = st.number_input("Age of the Person", min_value=1)
+
+                             
