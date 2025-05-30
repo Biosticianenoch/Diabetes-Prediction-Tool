27153f79dfa498e5ba738244c556e7003cd9859c
Enock Wafula Bereka <enochosenwafulah@gmail.com>
2025-05-30 21:18:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pickle
-import os
 
 st.title('Diabetes Prediction Tool')
 
 st.write('This tool is designed to make real world prediction of Diabetes Outcomes in Real world settings')
+
+# Load saved models
+diabetes_model = pickle.load(open(diabetes_model.sav"), 'rb')
